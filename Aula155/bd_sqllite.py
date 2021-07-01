152f51138558d19100029b6cd800d4eaeffd3b33
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-07-01 17:39:26 -0300

    move

--- Aula155/bd_sqllite.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conexao = sqlite3.connect('basededados.db')
-cursor = conexao.cursor()
-
-cursor.execute('CREATE TABLE IF NOT EXIST clientes ()')
-
-cursor.close()
-conexao.close()
